956722bec9b18464708df22461896f66d42c30a4
Joshua Triana <joshua.triana@correounivalle.edu.co>
2023-08-17 14:38:46 -0700

    integrar el tema de ia, mejorar el api

--- backend/api_control.py
+++ backend/api_control.py
@@ -3,7 +3,7 @@ from flask_restx import fields, Namespace
 # create an API namespace
 # Namespace is a group of API endpoints/routes like an MVC API controller
 taskCtrlr = Namespace(
-    'task', path="task", description='Task API Controller')
+    'task', path="/task", description='Task API Controller')
 
 # create API models for various api endpoints
 # the data type and data validation for each field in API model can be specified
@@ -11,12 +11,14 @@ taskCtrlr = Namespace(
 createTaskCommand = taskCtrlr.model('Create task command', {
     'value': fields.String(required=True, description='task description'),
     'order': fields.Integer(required=True, description='priority of task'),
+    'completed': fields.Boolean(required=True, description='task completion'),
     'list_id': fields.Integer(required=True, description='Task must be assigned to a List')
 })
 
 updateTaskCommand = taskCtrlr.model('Update Task command', {
     'value': fields.String(required=True, description='task description'),
     'order': fields.Integer(required=True, description='priority of task'),
+    'completed': fields.Boolean(required=True, description='task completion'),
     'list_id': fields.Integer(required=True, description='Task must be assigned to a List')
 })
 
@@ -24,12 +26,13 @@ taskDto = taskCtrlr.model('Task DTO', {
     'id': fields.Integer(description='Id of Task'),
     'value': fields.String(description='Task details'),
     'order': fields.Integer(description='Task order'),
+    'completed': fields.Boolean(required=True),
     'list_id': fields.Integer(description='Task must be assigned to a List')
 })
 
 # create an API namespace for list
 listCtrlr = Namespace(
-    'list', path="list", description='List API Controller')
+    'list', path="/list", description='List API Controller')
 
 # Models for listCtrlr
 createListCommand = listCtrlr.model('Create List command', {
@@ -43,4 +46,13 @@ updateListCommand = listCtrlr.model('Update List command', {
 listDto = listCtrlr.model('List DTO', {
     'id': fields.Integer(description='Id of List'),
     'title': fields.String(description='List details')    
+})
+
+# create an API for langchain
+langchainCtrlr = Namespace(
+    'langchain', path="/langchain", description='List API for Langchain')
+
+# Models for listCtrlr
+createlangChainCommand = langchainCtrlr.model('Create List command', {
+    'prompt': fields.String(required=True, description='template prompt'),    
 })
--- backend/config.py
+++ backend/config.py
@@ -3,11 +3,11 @@ import pathlib
 
 from flask import Flask
 from json import JSONEncoder
-from flask_cors import CORS
 
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 basedir = pathlib.Path(__file__).parent.resolve()
 
