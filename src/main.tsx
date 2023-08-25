2cd6c0c25ab898c10855f6cb988960e679545723
sebastiancastano1 <sebastian.castano@gorillalogic.com>
2023-08-25 16:40:33 -0500

    removing react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,16 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { Router } from "./Router";
-import { lightTheme } from "./themes";
 import { ThemeProvider } from "styled-components";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 6000,
-    },
-  },
-});
+import { Router } from "./Router";
+import { lightTheme } from "./themes";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
-      <QueryClientProvider client={queryClient}>
         <Router />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
