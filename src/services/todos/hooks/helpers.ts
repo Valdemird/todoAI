1bf68c579c6a878d1b9752438c8c3aa9c041a481
sebastiancastano1 <sebastian.castano@gorillalogic.com>
2023-08-26 08:36:38 -0500

    adding helpers

---
+++ src/services/todos/hooks/helpers.ts
@@ -0,0 +1,51 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+const showAlert = () => {
+  alert(`Error syncing with the server. Please try again later.`);
+};
+export const useMutationCreator = <T>(
+  fetchFunction: (arg: T) => Promise<T>,
+  getId: string[],
+  optimisticBehaviorCallback: (old: T[] | undefined, newElement: T) => T[]
+) => {
+  const queryCache = useQueryClient();
+  const addElementMutation = useMutation({
+    mutationFn: (newElement: T) => {
+      return fetchFunction(newElement);
+    },
+    onMutate: async (newElement) => {
+      await queryCache.cancelQueries({ queryKey: getId });
+      const previousElement = queryCache.getQueryData(getId);
+      queryCache.setQueryData(getId, (old: T[] | undefined) =>
+        optimisticBehaviorCallback(old, newElement)
+      );
+      return { previousElement };
+    },
+    onError: (_, __, context) => {
+      showAlert();
+      queryCache.setQueryData(getId, context?.previousElement);
+    },
+    onSettled: () => {
+      queryCache.invalidateQueries({ queryKey: getId });
+    },
+  });
+
+  return addElementMutation;
+};
+
+export const useMutationWithInvalidation = <T>(
+  fetchFunction: (arg: T) => Promise<void>,
+  getIds: string[]
+) => {
+  const queryCache = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: fetchFunction,
+    onError: () => {
+      showAlert();
+    },
+    onSuccess: () => {
+      queryCache.invalidateQueries({ queryKey: getIds });
+    },
+  });
+  return mutation;
+};
